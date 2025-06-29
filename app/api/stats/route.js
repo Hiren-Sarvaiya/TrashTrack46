45f33026f006f573eaf6b7ea585102968cdc9f4e
Hiren Sarvaiya <hirensarvaiya1612@gmail.com>
2025-06-30 01:48:15 +0530

    debugging api

--- app/api/stats/route.js
+++ app/api/stats/route.js
@@ -14,6 +14,6 @@ export async function GET() {
     }
     return NextResponse.json({ success: true, statsData })
   } catch (err) {
-    return NextResponse.json({ success: false, message: "Failed to count users" }, { status: 500 })
+    return NextResponse.json({ success: false, message: "Failed to count users", err: err }, { status: 500 })
   }
 }
